d3cbf7a4d2fef9a6f7279d1d6a7a1592a6cf7edd
Reasty Kim <reastykim@gmail.com>
2024-06-21 22:03:17 +0900

    Add project files.

---
+++ BlazorApp3/Components/NullableInputDate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorApp3.Components
+{
+    public class NullableInputDate<T> : InputDate<T>
+    {
+        protected override bool TryParseValueFromString(string value, out T result, out string validationErrorMessage)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                result = default;
+                validationErrorMessage = null;
+                return true;
+            }
+
+            return base.TryParseValueFromString(value, out result, out validationErrorMessage);
+        }
+    }
+}
---
+++ BlazorApp3/Data/Migrations/20240621125647_AddBirthday.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp3.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthday : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Birthday",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "AspNetUsers");
+        }
+    }
+}
